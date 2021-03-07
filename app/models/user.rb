eafbde99f5092bc755cf35d92516a3ce335d64a5
ryota-akamatsu <ra.ra.3.cp.3@gmail.com>
2021-03-08 06:04:23 +0900

    user管理機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,26 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  extend ActiveHash::Associations::ActiveRecordExtensions
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         has_many :Posts
+         belongs_to :sex
+         with_options presence: true do
+          validates :nickname
+          validates :name
+          validates :height
+          validates :age
+          validates :sex_id
+
+          validates  :name,
+          format: {with:/\A[ぁ-んァ-ン一-龥]+\z/}
+          validates  :height,
+          format: {with:/\A[0-9]+\z/}
+          validates  :age,
+          format: {with:/\A[0-9]+\z/}
+          validates :password,
+                 format:{with:/\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[\d])\w{6,12}\z/}
+          validates :sex_id, numericality: { other_than: 1 }
+          end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   get 'posts/index'
   root to: "posts#index"
+  resources :user
+  resources :posts
 end
